def97991a62543643504a5befda9dd51a06224fe
sailendrachettri <sailendra9083@gmail.com>
2024-10-23 22:31:10 +0530

    insertion sort

--- sorting/selectionSort.cpp
+++ sorting/selectionSort.cpp
@@ -12,7 +12,7 @@ void printArr(vector<int> arr){
 void selectionSort(vector<int> &arr){
     int size = arr.size();
 
-    for(int i = 0; i < size-1; i++){
+    for(int i = 0; i < size-1; i++){  
         int smallIndex = i;
 
         for(int j = i+1; j < size; j++){
