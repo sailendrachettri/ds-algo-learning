1770ac3a7930830cd1e1635ced6b9186986e4af4
sailendrachettri <sailendra9083@gmail.com>
2024-11-03 09:14:16 +0530

    contest 1 question solved

--- sorting/contest/weekly421.cpp
+++ sorting/contest/weekly421.cpp
@@ -1,35 +1,36 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    string s = "abcyy";  // Original string
-    int t = 2;           // Number of transformations
+int main()
+{
+    string s = "abcyy";
+    // string s = "azbk";
+    int t = 2;
     long long mod = 1000000007;
 
-    // Initialize size of the original string
-    long long size = s.size();
+    string temp = s;
 
-    // Count initial 'z' characters
-    long long z_count = 0;
-    for (char c : s) {
-        if (c == 'z') {
-            z_count++;
+    while (t > 0)
+    {
+        string str = "";
+        int size = temp.size();
+        
+        for (int i = 0; i < size; i++)
+        {
+            if (temp[i] == 'z')
+            {
+                str += "ab";
+                continue;
+            }
+
+            str += temp[i] + 1;
         }
+        temp = str;
+        t--;
     }
 
-    // Perform transformations
-    for (int i = 0; i < t; i++) {
-        // Each 'z' adds 1 to the size
-        size = (size + z_count) % mod;
+    cout << "temp: " << temp << endl;
+    cout << "temp size: " << temp.size()% mod << endl;
 
-        // After the transformation, we can only add new 'z' if the size is increased
-        // However, since we are only counting how many z's are there,
-        // We can safely update z_count to the new size for the next iteration.
-        // In the real transformation, the new z count would be based on the original string,
-        // but for our purpose, we know how many new z's would be created each time
-        // So we continue with our previous count.
-    }
-
-    cout << "Final size after transformations: " << size << endl;
     return 0;
 }
