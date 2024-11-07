45699d9cec4c5b233c2e796db9ae93718939c71c
sailendrachettri <sailendra9083@gmail.com>
2024-11-07 20:01:03 +0530

    folder structure added

---
+++ string/strings-replace/string_replace_2.cpp
@@ -0,0 +1,31 @@
+/*
+replace the space with %20
+
+Example:
+    str = "   I am     a good coder     "
+    output = "I%20am%20good%20coder";
+*/
+
+// Follow Up: can you come up with the O(1) space complexity
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+void replaceCh(string &s){
+
+   // code it here
+}
+
+int main(){
+
+    string str = "   I am     a good coder     ";
+
+    replaceCh(str);
+
+    cout << endl << str << endl;
+
+
+
+    return 0;
+}
