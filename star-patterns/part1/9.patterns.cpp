147101483f78f51db214f5e16414c98430394750
sailendrachettri <sailendra9083@gmail.com>
2024-11-10 10:49:05 +0530

    last changes

---
+++ star-patterns/part1/9.patterns.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int row = 5;
+
+    
+    for(int i = 1; i < row; i++){
+
+        for(int sp = row; sp >= i; sp--){
+            cout << "  ";
+        }
+
+        for(int j = 1; j <= (2*i-1); j++){
+            cout << "* ";
+        }
+        cout << endl;
+    }
+
+     for(int i = row; i > 1; i--){
+
+        for(int sp = row; sp >= i; sp--){
+            cout << "  ";
+        }
+
+        for(int j = 1; j <= (2*i-1); j++){
+            cout << "* ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
