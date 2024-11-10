9236a1f4dbfda9fd1efe18955bea67e81bbf4cd8
sailendrachettri <sailendra9083@gmail.com>
2024-11-10 14:28:25 +0530

    comnplex printing

--- number-patterns-part2/3.patterns.cpp
+++ number-patterns-part2/3.patterns.cpp
@@ -4,10 +4,50 @@ using namespace std;
 int main(){
 
     int row = 6;
+    int val = 1;
 
     cout << endl << endl;
 
-    
+    for(int i = 1; i <= row; i++){
+        for(int j = row; j >= i; j--){
+            cout << "* ";
+        }
+
+        for(int sp = 1; sp <= i; sp++){
+            cout << sp << " ";
+        }
+
+        for(int sp = i-1; sp >= 1; sp--){
+            cout << sp << " ";
+        }
+
+        for(int sp = row; sp >= i; sp--){
+            cout << "* ";
+        }
+
+        cout << endl;
+    }
+
+    for(int i = row-1; i >= 1; i--){
+        for(int j = row; j >= i; j--){
+            cout << "* ";
+        }
+
+        for(int sp = 1; sp <= i; sp++){
+            cout << sp << " ";
+        }
+
+        for(int sp = i-1; sp >= 1; sp--){
+            cout << sp << " ";
+        }
+
+        for(int sp = row; sp >= i; sp--){
+            cout << "* ";
+        }
+
+        cout << endl;
+    }
+
 
     cout << endl << endl;
 
