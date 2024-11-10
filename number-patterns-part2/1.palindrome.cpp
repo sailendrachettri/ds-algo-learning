aedd9907ae3c40037a4b4bd72a22f4df3e542ef6
sailendrachettri <sailendra9083@gmail.com>
2024-11-10 13:39:14 +0530

    pattern of number part 2

---
+++ number-patterns-part2/1.palindrome.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int row = 5;
+    
+
+    for(int i = 1; i <= row; i++){
+        for(int sp = row; sp >= i; sp--){
+            cout << "* ";
+        }
+
+         // Print the ascending part of the palindrome
+        for (int j = 1; j <= (2*i-1); j++) {
+            cout << j << " ";
+        }
+        
+        // Print the descending part of the palindrome
+        // for (int j = i - 1; j >= 1; j--) {
+        //     cout << j << " ";
+        // }
+
+        for(int sp = row; sp >= i; sp--){
+            cout << "* ";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
