ed84d46832337ac57108c653aa72e9553631a894
sailendrachettri <sailendra9083@gmail.com>
2024-11-19 21:52:06 +0530

    linked list insert at head and tail

--- linked-list/creating_a_linkedlist.cpp
+++ linked-list/creating_a_linkedlist.cpp
@@ -1,23 +1,61 @@
 #include <iostream>
 using namespace std;
 
-struct Node{
-    int data;
-    struct Node *next;
+class Node {
+    public:
+        int data;
+        Node * next;
 
-    Node (int d){
-        data = d;
-        next = NULL;
-    }
+        Node(int data){
+            this->data = data;
+            this->next = NULL;
+        }
 };
 
+void insertAtHead(Node *&head, int data){
+    Node *newNode = new Node(data);
+    newNode->next = head;
+    head = newNode;
+}
+
+void insertAtTail(Node* &tail, int data){
+    Node *newNode = new Node(data);
+    tail->next = newNode;
+    tail = newNode;
+}
+
+void printLL(Node * &head){
+    Node *temp = head;
+
+    while(temp != NULL){
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+
+    cout << endl;
+}
+
+
 int main(){
-    Node *head = new Node(10);
-    Node *temp1 = new Node(20);
-    Node *temp2 = new Node(40);
 
-    head->next = temp1;
-    temp1->next = temp2;
+    // Node *node1 = new Node(10);
+    // Node *head = node1;
 
+    // printLL(head);
+
+
+    // insertAtHead(head, 29);
+    // printLL(head);
+
+    // tail insertion
+
+    Node *node1 = new Node(10);
+    Node *head = node1;
+    Node *tail = node1;
+    insertAtTail(tail, 20);
+    insertAtTail(tail, 22);
+    printLL(head);
+  
+    
     return 0;
 }
