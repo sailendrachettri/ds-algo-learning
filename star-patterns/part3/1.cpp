db3bbf0e4985cfe2a26184eef3eb82dd7ee0298f
sailendrachettri <sailendra9083@gmail.com>
2024-12-01 21:05:00 +0530

    revision of patterns problems

---
+++ star-patterns/part3/1.cpp
@@ -0,0 +1,152 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int rows = 9;
+
+
+
+
+    
+
+
+
+
+    // for(int i = 1; i <= rows; i++){
+    //     for(int sp = rows; sp >= i; sp--){
+    //         cout << "  ";
+    //     }
+    //    for(int j = 1; j <= (2*i-1); j++){
+    //     cout << "* ";
+    //    }
+    //    cout << endl;
+    // }
+    // for(int i = rows-1; i >= 1; i--){
+    //     for(int sp = rows; sp >= i; sp--){
+    //         cout << "  ";
+    //     }
+    //    for(int j = 1; j <= (2*i-1); j++){
+    //     cout << "* ";
+    //    }
+    //    cout << endl;
+    // }
+
+
+
+
+
+
+
+
+
+
+
+    // for(int i = 1; i < rows; i++){
+    //     for(int sp = rows; sp >= i; sp--){
+    //         cout << "  ";
+    //     }
+    //    for(int j = 1; j <= (2*i-1); j++){
+    //     cout << "* ";
+    //    }
+    //    cout << endl;
+    // }
+
+
+
+
+
+
+    // for(int i = 1; i <= rows; i++){
+    //     for(int j = 1; j <= rows-i-1; j++){
+    //         cout << j << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+
+
+
+
+
+
+
+
+    // for(int i = rows; i >= 1; i--){
+    //     for(int j = i; j >= 1; j-- ){
+    //         cout << "*";
+    //     }
+    //     cout << endl;
+    // }
+
+
+
+
+
+
+    // for(int i = 1; i <= rows; i++){
+    //     for(int j = 1; j <= i; j++){
+    //         cout << i << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // for(int i = 1; i <= rows; i++){
+    //     for(int j = 1; j <= i; j++){
+    //         cout << j << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // for(int i = 0; i < rows; i++){
+    //     for(int j = 0; j <= i; j++){
+    //         cout << " * ";
+    //     }
+    //     cout << endl;
+    // }
+
+
+
+
+
+
+
+    // for(int i = 0; i < rows; i++){
+    //     for(int j = 0; j < rows; j++){
+    //         cout << " * ";
+    //     }
+    //     cout << endl;
+    // }
+
+    return 0;
+}
